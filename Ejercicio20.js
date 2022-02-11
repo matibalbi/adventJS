528fb4f6a1cec9ebd58ec74f59b2d16aa043a80f
Matias <matibalbi@gmail.com>
2022-02-11 13:51:32 -0300

    ej 20 corregido

--- Ejercicio20.js
+++ Ejercicio20.js
@@ -2,16 +2,17 @@ function pangram(letter) {
     var arr = []
     for (var i = 0; i < letter.length; i++) {
         var char = letter[i].toLowerCase()
-        if ((char.charCodeAt(0) < 97 || char.charCodeAt(0) > 122) && char !== "ñ") continue
-        if (char === "á" || char === "ä") char = "a"
-        if (char === "é" || char === "ë") char = "e"
-        if (char === "í" || char === "ï") char = "i"
-        if (char === "ó" || char === "ö") char = "o"
-        if (char === "ú" || char === "ü") char = "u"
+        if (char !== "ñ") {
+            char = char.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+            if (char.charCodeAt(0) < 97 || char.charCodeAt(0) > 122) continue
+        }
         if (!arr.includes(char)) arr.push(char)
     }
     return arr.length === 27
 }
+
+const str = "Crèmeáñ Brulée"
+console.log(str.normalize("NFD").replace(/[\u0300-\u036f]/g, ""))
 
 console.log(pangram('Extraño pan de col y kiwi se quemó bajo fugaz vaho')) // true
 console.log(pangram('Jovencillo emponzoñado y con walkman: ¡qué figurota exhibes!')) // true
